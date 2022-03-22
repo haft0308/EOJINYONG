2325f8c50585ec5c9d74d8d32c1694849931dfeb
EoJY <101792206+EoJY@users.noreply.github.com>
2022-03-22 14:18:00 +0900

    Add files via upload

--- home_study/Study_0322.java
+++ home_study/Study_0322.java
@@ -1,5 +1,5 @@
 package home_study;
-
+import java.util.Scanner;
 public class Study_0322 {
 
 	public static void main(String[] args) {
@@ -19,11 +19,20 @@ public class Study_0322 {
 	    	System.out.print(k);
 	    }
 		System.out.println();
-		for(k = 0; k < 10; k++)
+		for(k = 1; k < 10; k++)
 		{
 		    System.out.println("3단=3*"+k+"="+3*k);		
 		}
 	    
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("구하고 싶은 단수를 입력하세요. : ");
+		int input = sc.nextInt();
+		
+		for (k = 1 ; k < 10; k++)
+		{
+			System.out.println("입력받은 숫자의 단수 ="+input+"*"+k+"="+input*k);
+		}
 	}
 
 }
