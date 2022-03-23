1ecfd2a5af7c27625d30b02ca86305bd69714e26
EoJY <101792206+EoJY@users.noreply.github.com>
2022-03-23 16:33:37 +0900

    Add files via upload

---
+++ home_study/Homework_print.java
@@ -0,0 +1,207 @@
+package homework;
+
+public class Homework_print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		1번문제
+//
+//		int a = 0;
+//		for(a = 0; a < 10; a++)
+//		{
+//			System.out.println(a);
+//		}
+		
+		
+//2번문제		
+//		int sum = 0;
+//		int b = 0;
+//		int a[] = new int[6];
+//		System.out.println(a[b]);
+//		for(b = 0; b < 6; b++ )
+//		{
+//			sum = sum+a[b];
+//		}
+//		
+		
+		
+//3번문제		
+//		int num[]= new int[4];
+//		int a = 0;
+//		int b = 0;
+//		for(a = 10; a<=10000; a= a*10)
+//		{
+//			
+//			System.out.println("num["+ b +"]="+a);
+//			num[b++]=a;
+//		}
+//		
+		
+//4번문제
+//		int numb[]= new int[6];
+//		int a = 1;
+//		int b = 0;
+//		for(b = 0; b <6; b++)
+//		{
+//			
+//			System.out.println("numb["+ b +"]="+a);
+//			numb[b]=a;
+//		}
+//		System.out.println(numb[2]);
+		
+//5번문제
+//		int number[] = new int[6];
+//		int a = 0;
+//		int b = 0;
+//		for (a = 0; a < 6; a++)
+//		{
+//			
+//			System.out.println("number["+ a +"]="+b);
+//			number[b++]=a;
+//		}
+
+		
+//6번문제	
+//		int numbe[] = new int[6];
+//		int a = 0;
+//		int b = 0;
+//		for (a = 1; a <= 11; a = a+2)
+//		{
+//			
+//			System.out.println("numbe["+ b +"]="+a);
+//			numbe[b++]=a;
+//		}
+//		System.out.println(numbe[4]);
+		
+//7번문제
+//		int nu[] = new int[6];
+//		int a = 0;
+//		int b = 0;
+//		for (a = 10; a <= 60; a = a+10)
+//		{
+//			System.out.println("nu["+ b +"]="+a);
+//			nu[b++]=a;
+//		}
+//		System.out.println(nu[4]);
+		
+//8번문제
+//		int n[] = new int[5];
+//		int a = 0;
+//		int b = 1;
+//		for(a = 1; a <= 81; a=a+2 )
+//		{
+//			a=b*b;
+//			System.out.println("n["+ b +"]="+a);
+//			b=b+2;
+//		}
+		
+		
+		
+// 9번문제		
+//		int jungsu[] = new int [100];
+//		int a = 0;
+//		int b = 1;
+//		int sum = 0;
+//		for (a = 0; a<100; a++)
+//		{
+//			sum = sum + b;
+//			System.out.println("jungsu["+a+"]="+b);
+//			b++;
+//			
+//		}
+//		System.out.println(sum);
+		
+// 10번문제	
+//		int a[] = new int[] {2,10,8,5,3,9};
+//		int b = 0;
+//		int sum = 0;
+//		System.out.println(a[0]);
+//		for (b = 0; b < 6; b++)
+//		{
+//			sum = sum+a[b];
+//		}
+//		System.out.println(sum);
+		
+//11번문제
+//		char ch[] = new char[] {'a','p','p','l','e'};
+//		int a = 0;
+//		for (a = 0; a < 5; a++)
+//		{
+//			System.out.println(ch[a]);
+//		}
+//		for(a = 4; a >= 0; a--)
+//		{
+//			System.out.println(ch[a]);
+//		}
+		
+		
+//12번 문제
+//		int i[] = new int[10];
+//		
+//		int sum = 0;
+//		int a = 0;
+//		int b = 1;
+//		for (a = 0; a < 10; a++)
+//		{
+//			i[a]=b; 
+//			b = b*10;
+//		}
+//		for (a = 0; a < 10; a++)
+//		{
+//			sum = sum+i[a];
+//			
+//		}
+//		System.out.println(sum);
+
+		
+//13번 문제
+//		if(i[0]>i[1])
+//		{
+//			System.out.println(i[0]);
+//		}
+//		else
+//		{
+//			System.out.println(i[1]);
+//		}
+		
+//14번 문제
+//		if(i[0]>i[1]&&i[0]>i[2])
+//		{
+//			System.out.println(i[0]);		
+//		}
+//		else if (i[1]>i[0]&&i[1]>i[2])
+//		{
+//			System.out.println(i[1]);
+//		}
+//		else
+//		{
+//			System.out.println(i[2]);
+//		}
+		
+//15번 문제
+//		int []a = new int[10];
+//		int key = 8;
+//      //초기화
+//		for (int i = 0; i < 10; i++)
+//		{
+//			a[i]=i;
+//		}
+//		for (int i = 0; i < 10; i++)
+//		{
+//			System.out.println(a[i]);
+//			if (key == a[i])
+//			{
+//				System.out.println("key찾음");
+//				break;
+//			}
+//		}
+//      //key에 해당되는 값이 들어가면 멈추게 하는것. 패스워드 맞출때?
+		
+		
+//16번 문제		
+		
+		
+}
+
+}
---
+++ home_study/Homework_print_p20_start.java
@@ -0,0 +1,55 @@
+package homework;
+import java.util.Scanner;
+public class Homework_print_p20_start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		
+//16번 문제
+//		int a[] = new int[5];
+//		int b = 0;
+//		for(b = 0; b < 5; b++)
+//		{
+//			int input = sc.nextInt();
+//			a[b] = input; 
+//			
+//		}
+//		for(b = 0; b < 5; b++)
+//		{
+//			System.out.println(a[b]);
+//		}
+		
+		
+//17번 문제
+//		int intArr[] = new int[] {1,2,3,4,5};
+//		int i ;
+//		// 초기화 된 값 출력
+//		System.out.println("초기화 된 값 출력");
+//		for (i = 0; i < intArr.length; i++)
+//		{
+//			System.out.print(intArr[i]+"\t");
+//		}
+//		System.out.println();
+//		//출력결과를 유추하시오
+//		
+//		
+//		// 사용자한테 직접 숫자를 입력받아 배열에 저장
+//		System.out.println("숫자 다섯개를 입력하시오");
+//		for (i = 0; i < intArr.length; i++)
+//		{
+//			intArr[i] = sc.nextInt();
+//		}
+//		//배열의 저장된 값 출력
+//		System.out.println("배열의 저장된 값 출력");
+//		for(i = 0; i < intArr.length; i++)
+//		{
+//			System.out.print(intArr[i]+"\t");
+//		}
+//		
+		
+		
+	}
+
+}
